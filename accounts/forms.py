81132855847dbd6ba7c42f690472e840e4a02c01
Ryan <rmyoung04@gmail.com>
2025-03-23 17:16:24 -0500

    Fixed routing issue

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,7 +7,7 @@ The Meta class in Django forms is used to specify configurations
 for the form itself—essentially telling the form how it should behave and which model and fields it is associated with
 '''
 
-
+# https://docs.djangoproject.com/en/3.2/topics/auth/customizing/
 class CustomUserCreationForm(UserCreationForm):
     # Meta class overrides default fields
     class Meta(UserCreationForm.Meta):
